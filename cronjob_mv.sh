388aa2b639d950a7491dbc4c355169c0854fce3d
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-12-14 13:37:11 -0700

    Update cronjob_mv.sh

--- cronjob_mv.sh
+++ cronjob_mv.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+#create cron job:
+#crontab -e
+#m h      dom     mon   dow    command
+#0 14 * * * /home/<user>/scripts/cronjob_mv.sh
+#run the script at 2:00PM(1400) every day
 mv /home/<user>/testdir/* /home/<user>/archive
 echo "$(date) - Files backed up to archive by /home/<user>/scripts/cronjob_mv.sh" >> /home/<user>/logs/cronjob_mv.log
