32aa39561928f4c90efac702a36947879b5e7aab
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-12-17 13:00:30 -0700

    Create bash_format.sh

---
+++ bash_format.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# bash shell is located usually in /bin/bash
+
+echo "Display this to stdout"
+ls -lah
+
+#echo $PATH
+#show path for executable scripts and other files
+#export PATH=$PATH:/home/<user>/scripts
+#add scripts folder under home folder to path env variable
+
+#Conditionals and Loops
